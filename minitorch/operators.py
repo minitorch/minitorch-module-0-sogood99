4408b0463ede5c353822ecbd1b94eb08a8674295
Onii Chan <46487694+sogood99@users.noreply.github.com>
2024-09-16 00:03:12 -0400

    Basic operators

--- minitorch/operators.py
+++ minitorch/operators.py
@@ -35,6 +35,70 @@ from typing import Callable, Iterable
 # TODO: Implement for Task 0.1.
 
 
+def mul(x: float, y: float) -> float:
+    return x * y
+
+
+def id(x: float) -> float:
+    return x
+
+
+def add(x: float, y: float) -> float:
+    return x + y
+
+
+def neg(x: float) -> float:
+    return -x
+
+
+def lt(x: float, y: float) -> bool:
+    return x < y
+
+
+def eq(x: float, y: float) -> bool:
+    return x == y
+
+
+def max(x: float, y: float) -> float:
+    return x if x >= y else y
+
+
+def is_close(x: float, y: float) -> bool:
+    return abs(x - y) < 1e-2
+
+
+def sigmoid(x: float) -> float:
+    return 1.0 / (1.0 + math.exp(-x)) if x >= 0 else math.exp(x) / (1.0 + math.exp(x))
+
+
+def relu(x: float) -> float:
+    return x if x >= 0 else 0
+
+
+def log(x: float) -> float:
+    return math.log(x)
+
+
+def exp(x: float) -> float:
+    return math.exp(x)
+
+
+def inv(x: float) -> float:
+    return 1 / x
+
+
+def log_back(x: float, y: float) -> float:
+    return y / x
+
+
+def inv_back(x: float, y: float) -> float:
+    return -y / x**2
+
+
+def relu_back(x: float, y: float) -> float:
+    return y if x >= 0 else 0
+
+
 # ## Task 0.3
 
 # Small practice library of elementary higher-order functions.
@@ -49,6 +113,22 @@ from typing import Callable, Iterable
 # - addLists : add two lists together
 # - sum: sum lists
 # - prod: take the product of lists
+
+
+def negList(l: Iterable) -> Iterable:
+    return l
+
+
+def addLists(l: Iterable) -> Iterable:
+    return l
+
+
+def sumLists(l1: Iterable, l2: Iterable) -> Iterable:
+    return l1
+
+
+def prod(l1: Iterable, l2: Iterable) -> Iterable:
+    return l1
 
 
 # TODO: Implement for Task 0.3.
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -99,8 +99,8 @@ def test_eq(a: float) -> None:
 
 
 @pytest.mark.task0_2
-@given(small_floats)
-def test_sigmoid(a: float) -> None:
+@given(small_floats, small_floats)
+def test_sigmoid(a: float, b: float) -> None:
     """Check properties of the sigmoid function, specifically
     * It is always between 0.0 and 1.0.
     * one minus sigmoid is the same as sigmoid of the negative
@@ -108,7 +108,13 @@ def test_sigmoid(a: float) -> None:
     * It is  strictly increasing.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert 0.0 <= sigmoid(a) <= 1.0
+    assert_close(1 - sigmoid(a), sigmoid(-a))
+    assert sigmoid(0) == 0.5
+    if a < b:
+        assert sigmoid(a) <= sigmoid(b)
+    elif b < a:
+        assert sigmoid(b) <= sigmoid(a)
 
 
 @pytest.mark.task0_2
@@ -116,32 +122,36 @@ def test_sigmoid(a: float) -> None:
 def test_transitive(a: float, b: float, c: float) -> None:
     """Test the transitive property of less-than (a < b and b < c implies a < c)"""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    if lt(a, b) and lt(b, c):
+        assert lt(a, c)
 
 
 @pytest.mark.task0_2
-def test_symmetric() -> None:
+@given(small_floats, small_floats)
+def test_symmetric(a: float, b: float) -> None:
     """Write a test that ensures that :func:`minitorch.operators.mul` is symmetric, i.e.
     gives the same value regardless of the order of its input.
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert mul(a, b) == mul(b, a)
 
 
 @pytest.mark.task0_2
-def test_distribute() -> None:
+@given(small_floats, small_floats, small_floats)
+def test_distribute(a: float, b: float, c: float) -> None:
     r"""Write a test that ensures that your operators distribute, i.e.
     :math:`z \times (x + y) = z \times x + z \times y`
     """
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert_close(mul(a, add(b, c)), add(mul(a, b), mul(a, c)))
 
 
 @pytest.mark.task0_2
-def test_other() -> None:
+@given(small_floats, small_floats)
+def test_other(a: float, b: float) -> None:
     """Write a test that ensures some other property holds for your functions."""
     # TODO: Implement for Task 0.2.
-    raise NotImplementedError("Need to implement for Task 0.2")
+    assert max(a, b) == max(b, a)
 
 
 # ## Task 0.3  - Higher-order functions
